394d51252889f5ec1be1712d086c47800bd9a81e
Italovini223 <italovinicius2018@gmail.com>
2025-07-07 16:25:48 -0300

    🔥 removido procedimentos da view principal

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -27,7 +27,6 @@ public class Principal extends javax.swing.JFrame {
         jMenu1 = new javax.swing.JMenu();
         jmiDono = new javax.swing.JMenuItem();
         jmiPets = new javax.swing.JMenuItem();
-        jmiProcedimentos = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -65,14 +64,6 @@ public class Principal extends javax.swing.JFrame {
         });
         jMenu1.add(jmiPets);
 
-        jmiProcedimentos.setText("Procedimentos");
-        jmiProcedimentos.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jmiProcedimentosActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jmiProcedimentos);
-
         jMenuBar2.add(jMenu1);
 
         setJMenuBar(jMenuBar2);
@@ -109,12 +100,6 @@ public class Principal extends javax.swing.JFrame {
       
         
     }//GEN-LAST:event_jMenu1ActionPerformed
-
-    private void jmiProcedimentosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmiProcedimentosActionPerformed
-        ProcedimentosView procedimentos = new ProcedimentosView();
-        procedimentos.setVisible(true);
-        areaTrabalho.add(procedimentos);
-    }//GEN-LAST:event_jmiProcedimentosActionPerformed
 
     /**
      * @param args the command line arguments
@@ -157,6 +142,5 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar2;
     private javax.swing.JMenuItem jmiDono;
     private javax.swing.JMenuItem jmiPets;
-    private javax.swing.JMenuItem jmiProcedimentos;
     // End of variables declaration//GEN-END:variables
 }
